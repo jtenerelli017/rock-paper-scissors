6a1afc5d6c208555c5cb6b53469d9ce6e25c6b04
Justin Tenerelli <jtenerelli1@gmail.com>
2021-09-30 23:36:35 -0400

    Setting up various features for RPS game

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Jumbotron } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
 
+type UserAttack = "rock" | "paper" | "scissors";
 function App() {
-  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <button onClick={() => setCount(count + 1)}>Click me!</button>
-        <Jumbotron>{count}</Jumbotron>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Rock, Paper, Scissors!</h1>
+        <h2 className="App-buttons">
+          <Button variant='primary'>Rock!</Button>
+          <Button variant='primary'>Paper!</Button>
+          <Button variant='primary'>Scissors!</Button>
+        </h2>
       </header>
     </div>
   );
